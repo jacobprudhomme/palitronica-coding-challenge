ecaf38d585660413edf26f0161e2ab7ae6263b7e
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-01-31 18:08:37 -0500

    Added example for connecting to database in backend code

--- api/server.js
+++ api/server.js
@@ -1,14 +1,27 @@
 import Fastify from 'fastify';
+import fastifyPostgres from 'fastify-postgres';
 
 const server = Fastify({ logger: true });
 
-server.get('/', async (req, res) => (
-  { hello: 'world' }
-));
+server.register(fastifyPostgres, {
+  connectionString:
+    `postgres://${process.env.DB_USER || 'postgres'}@${process.env.DB_HOST || 'db'}/${process.env.DB_NAME || 'palitronica'}`,
+});
+
+server.get('/', async (req, _) => {
+  const dbClient = await server.pg.connect();
+  const { rows } = await dbClient.query(
+      'SELECT username FROM customers WHERE id=$1',
+      [req.params.id],
+    );
+  dbClient.release();
+
+  return rows;
+});
 
 async function start() {
   try {
-    await server.listen(process.env.PORT || 3000);
+    await server.listen(process.env.PORT || 3000, '0.0.0.0');
   } catch (err) {
     server.log.error(err);
     process.exit(1);
