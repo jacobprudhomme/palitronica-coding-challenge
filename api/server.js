930f610eec65a7af2f7a77acb5186863918ea3bf
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-01-31 20:28:00 -0500

    Added API endpoint for obtaining info for multiple items

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import fastifyPostgres from 'fastify-postgres';
+import format from 'pg-format';
 
 const server = Fastify({ logger: true });
 
@@ -17,6 +18,22 @@ server.get('/customers/:id', async (req, _) => {
 
   if (rows.length === 0) {
     throw { statusCode: 404, message: `Customer with id ${req.params.id} does not exist` };
+  } else {
+    return rows[0];
+  }
+});
+
+server.get('/items', async (req, _) => {
+  const ids = req.query.ids.split(',').map(id => parseInt(id));
+  const query = format('SELECT * FROM items WHERE id IN (%L)', ids);
+
+  const dbClient = await server.pg.connect();
+  const { rows } = await dbClient.query(query);
+
+  if (rows.length !== ids.length) {
+    const returnedIds = rows.map(row => row.id);
+    const missingIds = ids.filter(id => !returnedIds.includes(id));
+    throw { statusCode: 404, message: `Items with ids [${missingIds.join(',')}] do not exist` };
   } else {
     return rows;
   }
